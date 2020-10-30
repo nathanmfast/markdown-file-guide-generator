24cb01af30866232e116390c7580bc1e881c15db
Nathan Fast <nathanmfast@gmail.com>
2020-10-30 01:02:49 -0500

    i did it

--- src/readme-generator.ts
+++ src/readme-generator.ts
@@ -9,24 +9,20 @@ interface IFileOrFolder{
 
 class ReadmeGenerator {
   _folderNamesToIgnoreFilesIn: string[]
+  _maxLength: number
 
   constructor (folderNamesToIgnoreFilesIn?: string[]) {
     this._folderNamesToIgnoreFilesIn = folderNamesToIgnoreFilesIn
   }
 
   private getChildren (rootFolderPath: string): IFileOrFolder[] {
-    let children:IFileOrFolder[] = []
-    var dir = fs.readdirSync(rootFolderPath)
-    for (var item of dir) {
-      // get current file name
-      let name = path.parse(item).name
-      // get current file path
-      let filepath = path.resolve(rootFolderPath, item)
-      // get information about the file
-      let stat = fs.statSync(filepath)
-      // assemble IFileOrFolder
+    const children:IFileOrFolder[] = []
+    var files = fs.readdirSync(rootFolderPath)
+    for (var name of files) {
+      const filepath = path.resolve(rootFolderPath, name)
+      const stat = fs.statSync(filepath)
       let thisChildren: IFileOrFolder[] = []
-      let isFile = stat.isFile()
+      const isFile = stat.isFile()
       if (!isFile && !this._folderNamesToIgnoreFilesIn.includes(name)) {
         thisChildren = this.getChildren(filepath)
       }
@@ -36,45 +32,97 @@ class ReadmeGenerator {
         children: thisChildren
       })
     }
-    return children
+    children.sort((a, b) => {
+      if (!a.isFile && b.isFile) return -1
+      if (a.isFile && !b.isFile) return 1
+      return a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1
+    })
+
+    return children.map((x) => {
+      return {
+        ...x,
+        name: x.name + (x.isFile ? '' : '/')
+      }
+    })
   }
 
-  private getMaxLength (contents: IFileOrFolder[]): number{
-    //short-circuit
-    if(!contents || contents.length === 0){
-        return 0
+  private getMaxLength (contents: IFileOrFolder[], depth: number = 0): number {
+    if (!contents || contents.length === 0) {
+      return 0
     }
     let maxLength: number = 0
-    for(var item of contents){
-        let length:number = item.name.length
-        if(length> maxLength){
-            maxLength = length
-        }
-        let childrenMaxLength: number = this.getMaxLength(item.children)
-        if(childrenMaxLength> maxLength){
-            maxLength= childrenMaxLength
-        }
+    for (var item of contents) {
+      const length:number = item.name.length + (depth * 2) // however many characters you need for each nested folder
+      if (length > maxLength) {
+        maxLength = length
+      }
+      const childrenMaxLength: number = this.getMaxLength(item.children, depth + 1)
+      if (childrenMaxLength > maxLength) {
+        maxLength = childrenMaxLength
+      }
     }
     return maxLength
   }
 
+
+
+  private getLeader(depth: number, isLast: boolean): string{
+    let leader = ''
+    if(depth > 0){
+    if(depth > 1){
+      let numSpacer = depth - 1
+      for (var x = 0; x < numSpacer; x++) {
+        leader += '│  '
+      }
+    }
+    leader += isLast ? '└' : '├'
+    leader += '─ '
+  }
+    return leader
+    /*
+    switch(depth){
+      case 0:
+        return ''
+      case 1:
+        return 'xx '
+      case 2:
+        return 'x   xx '
+      case 3:
+        return 'x   x   xx '
+      default:
+        return 'FUCK'
+    }
+    */
+
+  }
+
+  private generateRow (contents: Array<IFileOrFolder>, depth: number = 0) : string {
+    let markdown :string = ''
+    let iterations = contents.length;
+    for (const item of contents) {
+      iterations--
+      markdown += '| '
+
+      let leader = this.getLeader(depth, !iterations)
+      //markdown += ''.padEnd(depth * 2)
+      markdown += leader
+
+      markdown += item.name.padEnd(this._maxLength - leader.length + 2, ' ')
+      markdown += '|'
+      markdown += ' |'
+      markdown += '\n'
+      markdown += this.generateRow(item.children, depth + 1)
+    }
+    return markdown
+  }
+
   public generate (rootFolderPath: string): string {
     const contents: IFileOrFolder[] = this.getChildren(rootFolderPath)
-
     console.log(contents)
-    let markdown: string = ''
 
-    //TODO: Generate that markdown table now
-    let maxLength: number = this.getMaxLength(contents)
-    for(var item of contents){
-        markdown += '| '
-        markdown += item.name.padEnd(maxLength)
-        markdown += ' |'
-        markdown += ' |'
-        markdown += '\n'
-    }
-    
-    return markdown
+    this._maxLength = this.getMaxLength(contents)
+
+    return this.generateRow(contents)
   }
 }
 
