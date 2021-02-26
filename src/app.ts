fbdde5fdfc8e6400959a0d99ed6eb7d7aee10e69
Nathan Fast <nathanmfast@gmail.com>
2021-02-25 21:34:18 -0600

    command line args demo

--- src/app.ts
+++ src/app.ts
@@ -2,3 +2,8 @@ import { generate } from './generator'
 
 const markdown = generate('C:\\Projects\\markdown-file-guide-generator', ['node_modules', 'dist', '.git'])
 console.log(markdown)
+
+// this is how command line arguments work for a node app
+process.argv.forEach((val, index) => {
+    console.log(`${index}: ${val}`)
+})
