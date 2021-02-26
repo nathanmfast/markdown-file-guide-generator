9ae05ede076b34e2948e3c8d97f442a8e02094af
Nathan Fast <nathanmfast@gmail.com>
2021-02-25 22:07:49 -0600

    linted and added default/alias

--- dist/app.js
+++ dist/app.js
@@ -3,11 +3,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const minimist = require("minimist");
 const generator_1 = require("./generator");
 var opts = {
-    string: ['rootFolderPath', 'column1Heading', 'column2Heading']
+    string: ['rootFolderPath', 'column1Heading', 'column2Heading'],
+    default: { rootFolderPath: './' },
+    alias: { path: 'rootFolderPath' }
 };
 var args = minimist(process.argv.slice(2), opts);
-const markdown = generator_1.generate(args.rootFolderPath ?? './', args._ ?? [], args.column1Heading, args.column2Heading);
+const markdown = generator_1.generate(args.rootFolderPath, args._ ?? [], args.column1Heading, args.column2Heading);
 console.log(markdown);
 // try running 'npm run build' then from project root run:
-// node ./dist/app.js --column1Heading=Test1 --column2Heading=Test2 node_modules dist .git 
+// node ./dist/app.js --column1Heading=Test1 --column2Heading=Test2 node_modules dist .git
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,13 @@ import * as minimist from 'minimist'
 import { generate } from './generator'
 
 var opts = {
-    string: ['rootFolderPath', 'column1Heading', 'column2Heading']
-};
-var args = minimist(process.argv.slice(2), opts);
-const markdown = generate(args.rootFolderPath ?? './', args._ ?? [], args.column1Heading, args.column2Heading);
-console.log(markdown);
+  string: ['rootFolderPath', 'column1Heading', 'column2Heading'],
+  default: { rootFolderPath: './' },
+  alias: { path: 'rootFolderPath' }
+}
+var args = minimist(process.argv.slice(2), opts)
+const markdown = generate(args.rootFolderPath, args._ ?? [], args.column1Heading, args.column2Heading)
+console.log(markdown)
 
 // try running 'npm run build' then from project root run:
-// node ./dist/app.js --column1Heading=Test1 --column2Heading=Test2 node_modules dist .git 
+// node ./dist/app.js --column1Heading=Test1 --column2Heading=Test2 node_modules dist .git
