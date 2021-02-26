c634874b248ec1989bc53b1496c4158deac05311
Nathan Fast <nathanmfast@gmail.com>
2021-02-25 21:40:32 -0600

    demo of minimist in action, ready to use it now

--- dist/app.js
+++ dist/app.js
@@ -3,4 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const generator_1 = require("./generator");
 const markdown = generator_1.generate('C:\\Projects\\markdown-file-guide-generator', ['node_modules', 'dist', '.git']);
 console.log(markdown);
+// this is how command line arguments work when using minimist
+console.log('--minimist--');
+var argv = require('minimist')(process.argv.slice(2));
+console.log(argv);
+console.log(argv.test);
+// try running 'npm run build' then 'node ./app.js --test=derp' to see it in action
 //# sourceMappingURL=app.js.map
