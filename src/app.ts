51e65ad3ab228964eeb568de6669106b15f5d019
Nathan Fast <nathanmfast@gmail.com>
2021-02-25 21:43:53 -0600

    better example of minimist in action with proper es6 imports

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
 import { generate } from './generator'
+import * as minimist from 'minimist'
 
 const markdown = generate('C:\\Projects\\markdown-file-guide-generator', ['node_modules', 'dist', '.git'])
 console.log(markdown)
 
 // this is how command line arguments work when using minimist
 console.log('--minimist--');
-var argv = require('minimist')(process.argv.slice(2));
-//console.log(argv);
-console.log(argv.test);
+var args = minimist(process.argv.slice(2));
+console.log(args.test);
 
 // try running 'npm run build' then 'node ./app.js --test=derp' to see it in action
